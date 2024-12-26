a7c684fc9761d3fe3d8726c05d1bcf9c1a74fd60
Ed Kaim <ed@sharplogic.com>
2024-12-25 23:52:21 -0500

    Add paging to notification UX

--- src/lightnap-ng/src/app/profile/components/pages/notifications/notifications.component.ts
+++ src/lightnap-ng/src/app/profile/components/pages/notifications/notifications.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { Router } from "@angular/router";
-import { ApiResponseComponent, ErrorListComponent, ToastService } from "@core";
+import { ApiResponseComponent, EmptyPagedResponse, ErrorListComponent, ToastService } from "@core";
 import { NotificationData, NotificationService } from "@profile";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
-import { TableModule } from "primeng/table";
-import { map, Observable } from "rxjs";
+import { TableLazyLoadEvent, TableModule } from "primeng/table";
+import { debounceTime, map, Observable, startWith, Subject, switchMap } from "rxjs";
 
 @Component({
   standalone: true,
@@ -14,20 +15,29 @@ import { map, Observable } from "rxjs";
   imports: [CommonModule, TableModule, ButtonModule, ErrorListComponent, CardModule, ApiResponseComponent],
 })
 export class NotificationsComponent {
+  readonly pageSize = 10;
+
   readonly #notificationService = inject(NotificationService);
   readonly #toast = inject(ToastService);
   readonly #router = inject(Router);
 
-  notifications$: Observable<Array<NotificationData>>;
+  #lazyLoadEventSubject = new Subject<TableLazyLoadEvent>();
+  notifications$ = this.#lazyLoadEventSubject.pipe(
+    switchMap(event =>
+      this.#notificationService.searchNotifications({
+        pageSize: this.pageSize,
+        pageNumber: event.first / this.pageSize + 1,
+      })
+    ),
+    // We need to bootstrap the p-table with a response to get the whole process running. We do it this way to
+    // fake an empty response so we can avoid a redundant call to the API.
+    startWith(new EmptyPagedResponse<NotificationData>())
+  );
 
   errors = new Array<string>();
 
-  constructor() {
-    this.#refresh();
-  }
-
-  #refresh() {
-    this.notifications$ = this.#notificationService.searchNotifications({}).pipe(map(results => results.data));
+  onLazyLoad(event: TableLazyLoadEvent) {
+    this.#lazyLoadEventSubject.next(event);
   }
 
   notificationClicked(notification: NotificationData) {
@@ -39,7 +49,7 @@ export class NotificationsComponent {
     this.#notificationService.markAllNotificationsAsRead().subscribe({
       next: () => {
         this.#toast.success("All notifications marked as read.");
-        this.#refresh();
+        this.#lazyLoadEventSubject.next({ first: 0 });
       },
       error: response => (this.errors = response.errorMessages),
     });
