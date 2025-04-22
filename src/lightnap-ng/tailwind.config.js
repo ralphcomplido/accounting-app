7347874b72da817f35e10eb3f4540d2c946e37c7
Ed Kaim <ed@sharplogic.com>
2025-04-21 17:04:18 -0700

    Style and test cleanup

--- src/lightnap-ng/tailwind.config.js
+++ src/lightnap-ng/tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-const primeui = require('tailwindcss-primeui');
 module.exports = {
-    darkMode: ['selector', '[class="app-dark"]'],
-    content: ['./src/**/*.{html,ts,css}', './index.html'],
-    plugins: [primeui],
+    darkMode: ["selector", '[class="app-dark"]'],
+    content: ["./src/**/*.{html,ts,css}", "./index.html"],
+    plugins: [require("tailwindcss-primeui")],
     theme: {
         screens: {
-            sm: '576px',
-            md: '768px',
-            lg: '992px',
-            xl: '1200px',
-            '2xl': '1920px'
-        }
-    }
+            sm: "576px",
+            md: "768px",
+            lg: "992px",
+            xl: "1200px",
+            "2xl": "1920px",
+        },
+    },
 };
