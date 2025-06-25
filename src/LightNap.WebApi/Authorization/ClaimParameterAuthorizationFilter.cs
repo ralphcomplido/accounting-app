dab9490a5bc18639de0627d1016f79d6474f254b
Ed Kaim <ed@sharplogic.com>
2025-06-25 14:30:15 -0700

    Migrate from handler to filter for param checks

---
+++ src/LightNap.WebApi/Authorization/ClaimParameterAuthorizationFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LightNap.WebApi.Authorization
+{
+    public class ClaimParameterAuthorizationFilter(string claimType, string parameterKey = "id", ClaimParameterSource source = ClaimParameterSource.Route) : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            string? parameter = source switch
+            {
+                ClaimParameterSource.Route =>
+                    context.HttpContext.Request.RouteValues.TryGetValue(parameterKey, out var routeValue) && routeValue != null
+                        ? routeValue.ToString()
+                        : null,
+
+                ClaimParameterSource.Query =>
+                    context.HttpContext.Request.Query.TryGetValue(parameterKey, out var queryValue)
+                        ? queryValue.FirstOrDefault()
+                        : null,
+
+                ClaimParameterSource.Literal => parameterKey,
+
+                _ => throw new ArgumentOutOfRangeException(nameof(source), source, $"Unsupported ClaimParameterSource")
+            };
+
+            if (string.IsNullOrEmpty(parameter) || !context.HttpContext.User.HasClaim(claimType, parameter))
+            {
+                context.Result = new ForbidResult();
+                return;
+            }
+        }
+    }
+}
--- src/LightNap.WebApi/Authorization/ClaimParameterSource.cs
+++ src/LightNap.WebApi/Authorization/ClaimParameterSource.cs
@@ -4,5 +4,6 @@
     {
         Route,
         Query,
+        Literal,
     }
 }
